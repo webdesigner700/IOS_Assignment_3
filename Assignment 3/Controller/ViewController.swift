d1450cf98d5e88d460a8988acbb3dcb8e753a438
Grace <solibungrace@gmail.com>
2023-05-13 22:58:44 +1000

    few fixes

--- Assignment 3/Controller/ViewController.swift
+++ Assignment 3/Controller/ViewController.swift
@@ -5,107 +5,38 @@
 //  Created by vinay bayyapunedi on 08/05/23.
 //
 
-//import Foundation
-//import UIKit
-//
-//class ViewController: UIViewController {
-//
-//    @IBOutlet weak var mainLabel: UILabel!
-//
-//    @IBOutlet weak var userNameTextField: UITextField!
-//
-//    @IBOutlet weak var passwordTextField: UITextField!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//        self.hideKeyboardWhenTappedAround()
-//    }
-//
-//    // we have to send the uername to the home screen
-//
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if segue.identifier == "goToHomePage" {
-//            let VC = segue.destination as! HomeScreenViewController
-//            VC.userName = userNameTextField.text!
-//        }
-//    }
-//
-//
-//}
-//
-//extension UIViewController {
-//    func hideKeyboardWhenTappedAround() {
-//        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-//        tap.cancelsTouchesInView = false
-//        view.addGestureRecognizer(tap)
-//    }
-//
-//    @objc func dismissKeyboard() {
-//        view.endEditing(true)
-//    }
-//}
-
-
 import Foundation
 import UIKit
 
-class LoginViewController: UIViewController {
+class ViewController: UIViewController {
 
     @IBOutlet weak var mainLabel: UILabel!
+
     @IBOutlet weak var userNameTextField: UITextField!
+
     @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         self.hideKeyboardWhenTappedAround()
     }
 
-    @IBAction func loginButtonTapped(_ sender: Any) {
-        guard let username = userNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
-              let password = passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
-              !username.isEmpty, !password.isEmpty else {
-            showAlert(message: "Please enter both username and password.")
-            return
-        }
-
-        let user = User(username: username, password: password, planIDs: [])
-        let dataStore = UserStore()
-        if let existingUser = dataStore.getUser(username: username) {
-            // User exists, validate password
-            if existingUser.password == user.password {
-                // Successful login
-                performSegue(withIdentifier: "goToHomePage", sender: nil)
-            } else {
-                // Invalid password
-                showAlert(message: "Invalid password.")
-            }
-        } else {
-            // User does not exist, add to datastore
-            dataStore.addUser(user: user)
-            performSegue(withIdentifier: "goToHomePage", sender: nil)
-        }
-    }
+    // we have to send the uername to the home screen
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToHomePage" {
-            guard let destinationVC = segue.destination as? HomeScreenViewController,
-                  let username = userNameTextField.text else { return }
-            destinationVC.userName = username
+            let VC = segue.destination as! HomeScreenViewController
+            VC.userName = userNameTextField.text!
         }
     }
 
-    private func showAlert(message: String) {
-        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(alertAction)
-        present(alertController, animated: true, completion: nil)
-    }
+
 }
 
 extension UIViewController {
     func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
@@ -115,22 +46,91 @@ extension UIViewController {
     }
 }
 
-//struct User {
-//    let username: String
-//    let password: String
-//    let planIDs: [String]
+
+//import Foundation
+//import UIKit
+//
+//class LoginViewController: UIViewController {
+//
+//    @IBOutlet weak var mainLabel: UILabel!
+//    @IBOutlet weak var userNameTextField: UITextField!
+//    @IBOutlet weak var passwordTextField: UITextField!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.hideKeyboardWhenTappedAround()
+//    }
+//
+//    @IBAction func loginButtonTapped(_ sender: Any) {
+//        guard let username = userNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+//              let password = passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+//              !username.isEmpty, !password.isEmpty else {
+//            showAlert(message: "Please enter both username and password.")
+//            return
+//        }
+//
+//        let user = User(username: username, password: password, planIDs: [])
+//        let dataStore = UserStore()
+//        if let existingUser = dataStore.getUser(username: username) {
+//            // User exists, validate password
+//            if existingUser.password == user.password {
+//                // Successful login
+//                performSegue(withIdentifier: "goToHomePage", sender: nil)
+//            } else {
+//                // Invalid password
+//                showAlert(message: "Invalid password.")
+//            }
+//        } else {
+//            // User does not exist, add to datastore
+//            dataStore.addUser(user: user)
+//            performSegue(withIdentifier: "goToHomePage", sender: nil)
+//        }
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "goToHomePage" {
+//            guard let destinationVC = segue.destination as? HomeScreenViewController,
+//                  let username = userNameTextField.text else { return }
+//            destinationVC.userName = username
+//        }
+//    }
+//
+//    private func showAlert(message: String) {
+//        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+//        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//        alertController.addAction(alertAction)
+//        present(alertController, animated: true, completion: nil)
+//    }
 //}
-
-class UserStore {
-    private var users: [User] = []
-
-    func addUser(user: User) {
-        users.append(user)
-    }
-
-    func getUser(username: String) -> User? {
-        return users.first(where: { $0.username == username })
-    }
-}
+//
+//extension UIViewController {
+//    func hideKeyboardWhenTappedAround() {
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+//        tap.cancelsTouchesInView = false
+//        view.addGestureRecognizer(tap)
+//    }
+//
+//    @objc func dismissKeyboard() {
+//        view.endEditing(true)
+//    }
+//}
+//
+////struct User {
+////    let username: String
+////    let password: String
+////    let planIDs: [String]
+////}
+//
+//class UserStore {
+//    private var users: [User] = []
+//
+//    func addUser(user: User) {
+//        users.append(user)
+//    }
+//
+//    func getUser(username: String) -> User? {
+//        return users.first(where: { $0.username == username })
+//    }
+//}
 
 
--- DataStore.swift
+++ DataStore.swift
@@ -7,6 +7,101 @@
 
 
 
+import Foundation
+
+struct Plan: Codable {
+
+    var planID: Int
+    var planName: String
+    var amount: Int
+    var transactionTime: Int
+    var paymentType: String
+}
+
+struct User: Codable {
+    var username: String
+    var password: String
+    var planIDs: [Int] // stores the IDs of the plans associated with the user
+}
+
+class DataStore {
+
+    static let shared = DataStore()
+
+    var storedPlans: [Plan] = [
+        Plan(planID: 0, planName: "plan1", amount: 60, transactionTime: 6, paymentType: "Cash"),
+        Plan(planID: 1, planName: "plan2", amount: 40, transactionTime: 2, paymentType: "Card")
+    ]
+
+    var buttonIDs: [Int] = []
+
+    var planID: Int = 2
+
+    var descriptionPlan: Plan = Plan(planID: 0, planName: "Dummy", amount: 0, transactionTime: 0, paymentType: "Dummy")
+
+    var timeframe: String = "Weekly"
+    var timeSelecterLocation: Int = 2
+
+    var allowanceNote: String?
+
+    private init(){
+        // put stuff that need to be initialised when the program start running
+        timeframe = "Weekly"
+    }
+
+    func findPlanByID(ID: Int) -> Plan {
+
+        for plan in storedPlans {
+            if (plan.planID == ID) {
+                return plan
+            }
+        }
+        return Plan(planID: 0, planName: "Error", amount: 0, transactionTime: 0, paymentType: "Error")
+    }
+
+    func addNewPlan(name: String, money: Int, time: Int, payType: String) {
+        storedPlans.append(Plan(planID: planID, planName: name, amount: money, transactionTime: time, paymentType: payType))
+        planID += 1
+
+    }
+
+    func getPlanStoredLocation(planID: Int) -> Int {
+        guard storedPlans.count > 1 else {
+            return -1 // return something that is out of bound to prevent incorrect logic operation in main menu
+        }
+
+        for index in (0...storedPlans.count - 1) {
+            if storedPlans[index].planID == planID {
+                return index
+            }
+        }// end of for loop
+
+        return 0
+    }
+
+    func removePlanByID(planID: Int) {
+        guard storedPlans.count > 0 else {
+            return
+        }
+
+        for index in (0...storedPlans.count - 1) {
+
+            if storedPlans[index].planID == planID {
+                storedPlans.remove(at: index)
+                //self.planID -= 1
+                return
+            }
+        }// end of for loop
+    }
+
+    func addPlanToUser(user: inout User, planID: Int) {
+        user.planIDs.append(planID)
+    }
+
+}
+
+
+
 //import Foundation
 //
 //struct Plan: Codable {
@@ -43,6 +138,8 @@
 //    var timeSelecterLocation: Int = 2
 //
 //    var allowanceNote: String?
+//
+//    var currentUser: User? = nil
 //
 //    private init(){
 //        // put stuff that need to be initialised when the program start running
@@ -98,169 +195,72 @@
 //        user.planIDs.append(planID)
 //    }
 //
+//    func authenticateUser(username: String, password: String) -> Bool {
+//        guard let users = loadUsers() else { return false }
+//
+//        for user in users {
+//            if user.username == username && user.password == password {
+//                currentUser = user
+//                return true
+//            }
+//        }
+//
+//        return false
+//    }
+//
+//    func loadUsers() -> [User]? {
+//        if let data = UserDefaults.standard.data(forKey: "users"),
+//           let users = try? PropertyListDecoder().decode([User].self, from: data) {
+//            return users
+//        }
+//        return nil
+//    }
+//
+//    func saveUsers(_ users: [User]) {
+//        let data = try? PropertyListEncoder().encode(users)
+//        UserDefaults.standard.set(data, forKey: "users")
+//    }
+//
+//    func loadCurrentUser() -> User? {
+//        if let data = UserDefaults.standard.data(forKey: "currentUser"), let user = try? PropertyListDecoder().decode(User.self, from: data) {
+//            return user
+//        }
+//        return nil
+//    }
+//
+//    func saveCurrentUser(_ user: User) {
+//        let data = try? PropertyListEncoder().encode(user)
+//        UserDefaults.standard.set(data, forKey: "currentUser")
+//    }
+//
+//    func login(username: String, password: String) -> Bool {
+//        if let users = loadUsers() {
+//            for user in users {
+//                if user.username == username && user.password == password {
+//                    saveCurrentUser(user)
+//                    return true
+//                }
+//            }
+//        }
+//        return false
+//    }
+//
+//    func logout() {
+//        UserDefaults.standard.removeObject(forKey: "currentUser")
+//    }
+//
+//    func getPlansForCurrentUser() -> [Plan] {
+//        var plansForUser: [Plan] = []
+//        if let currentUser = loadCurrentUser() {
+//            for planID in currentUser.planIDs {
+//                let plan = DataStore.shared.findPlanByID(ID: planID)
+//                plansForUser.append(plan)
+//            }
+//        }
+//        return plansForUser
+//    }
+//
 //}
-
-
-
-import Foundation
-
-struct Plan: Codable {
-    
-    var planID: Int
-    var planName: String
-    var amount: Int
-    var transactionTime: Int
-    var paymentType: String
-}
-
-struct User: Codable {
-    var username: String
-    var password: String
-    var planIDs: [Int] // stores the IDs of the plans associated with the user
-}
-
-class DataStore {
-    
-    static let shared = DataStore()
-    
-    var storedPlans: [Plan] = [
-        Plan(planID: 0, planName: "plan1", amount: 60, transactionTime: 6, paymentType: "Cash"),
-        Plan(planID: 1, planName: "plan2", amount: 40, transactionTime: 2, paymentType: "Card")
-    ]
-    
-    var buttonIDs: [Int] = []
-    
-    var planID: Int = 2
-    
-    var descriptionPlan: Plan = Plan(planID: 0, planName: "Dummy", amount: 0, transactionTime: 0, paymentType: "Dummy")
-    
-    var timeframe: String = "Weekly"
-    var timeSelecterLocation: Int = 2
-    
-    var allowanceNote: String?
-    
-    var currentUser: User? = nil
-    
-    private init(){
-        // put stuff that need to be initialised when the program start running
-        timeframe = "Weekly"
-    }
-    
-    func findPlanByID(ID: Int) -> Plan {
-        
-        for plan in storedPlans {
-            if (plan.planID == ID) {
-                return plan
-            }
-        }
-        return Plan(planID: 0, planName: "Error", amount: 0, transactionTime: 0, paymentType: "Error")
-    }
-    
-    func addNewPlan(name: String, money: Int, time: Int, payType: String) {
-        storedPlans.append(Plan(planID: planID, planName: name, amount: money, transactionTime: time, paymentType: payType))
-        planID += 1
-        
-    }
-    
-    func getPlanStoredLocation(planID: Int) -> Int {
-        guard storedPlans.count > 1 else {
-            return -1 // return something that is out of bound to prevent incorrect logic operation in main menu
-        }
-        
-        for index in (0...storedPlans.count - 1) {
-            if storedPlans[index].planID == planID {
-                return index
-            }
-        }// end of for loop
-        
-        return 0
-    }
-    
-    func removePlanByID(planID: Int) {
-        guard storedPlans.count > 0 else {
-            return
-        }
-        
-        for index in (0...storedPlans.count - 1) {
-            
-            if storedPlans[index].planID == planID {
-                storedPlans.remove(at: index)
-                //self.planID -= 1
-                return
-            }
-        }// end of for loop
-    }
-    
-    func addPlanToUser(user: inout User, planID: Int) {
-        user.planIDs.append(planID)
-    }
-    
-    func authenticateUser(username: String, password: String) -> Bool {
-        guard let users = loadUsers() else { return false }
-        
-        for user in users {
-            if user.username == username && user.password == password {
-                currentUser = user
-                return true
-            }
-        }
-        
-        return false
-    }
-    
-    func loadUsers() -> [User]? {
-        if let data = UserDefaults.standard.data(forKey: "users"),
-           let users = try? PropertyListDecoder().decode([User].self, from: data) {
-            return users
-        }
-        return nil
-    }
-    
-    func saveUsers(_ users: [User]) {
-        let data = try? PropertyListEncoder().encode(users)
-        UserDefaults.standard.set(data, forKey: "users")
-    }
-    
-    func loadCurrentUser() -> User? {
-        if let data = UserDefaults.standard.data(forKey: "currentUser"), let user = try? PropertyListDecoder().decode(User.self, from: data) {
-            return user
-        }
-        return nil
-    }
-    
-    func saveCurrentUser(_ user: User) {
-        let data = try? PropertyListEncoder().encode(user)
-        UserDefaults.standard.set(data, forKey: "currentUser")
-    }
-    
-    func login(username: String, password: String) -> Bool {
-        if let users = loadUsers() {
-            for user in users {
-                if user.username == username && user.password == password {
-                    saveCurrentUser(user)
-                    return true
-                }
-            }
-        }
-        return false
-    }
-    
-    func logout() {
-        UserDefaults.standard.removeObject(forKey: "currentUser")
-    }
-
-    func getPlansForCurrentUser() -> [Plan] {
-        var plansForUser: [Plan] = []
-        if let currentUser = loadCurrentUser() {
-            for planID in currentUser.planIDs {
-                let plan = DataStore.shared.findPlanByID(ID: planID)
-                plansForUser.append(plan)
-            }
-        }
-        return plansForUser
-    }
-    
-}
 
 
 
